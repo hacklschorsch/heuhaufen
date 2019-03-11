ea84ab5e08bd7a480003995c0624a447809189ba
Florian Sesser <fs-priv@fs-x201.cyberpunk.sytes.net>
2019-03-11 17:09:54 +0100

    Have test with fixture

--- test/heuhaufen/core_test.clj
+++ test/heuhaufen/core_test.clj
@@ -1,7 +1,19 @@
 (ns heuhaufen.core-test
   (:require [clojure.test :refer :all]
-            [heuhaufen.core :refer :all]))
+            [heuhaufen.core :refer :all]
+            [clucy.core :as clucy]))
+
+(defn setup-clojure
+  [f]
+  (def index (clucy/memory-index))
+  (clucy/add index
+       {:name "Bob", :job "Builder"}
+       {:name "Donald", :job "Computer Scientist"})
+  (f)
+  )
+
+(use-fixtures :each setup-clojure)
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Storing and retrieving things with Lucene"
+    (is (= (clucy/search index "bob" 10) '({:name "Bob", :job "Builder"})))))
