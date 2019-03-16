ae0658823ff31f07b5fac86c3de4348808261f67
Florian Sesser <fs-priv@fs-x201.cyberpunk.sytes.net>
2019-03-16 15:48:55 +0100

    Move search engine test to own file

--- test/heuhaufen/core_test.clj
+++ test/heuhaufen/core_test.clj
@@ -1,19 +1,7 @@
 (ns heuhaufen.core-test
   (:require [clojure.test :refer :all]
-            [heuhaufen.core :refer :all]
-            [clucy.core :as clucy]))
-
-(defn setup-clojure
-  [f]
-  (def index (clucy/memory-index))
-  (clucy/add index
-       {:name "Bob", :job "Builder"}
-       {:name "Donald", :job "Computer Scientist"})
-  (f)
-  )
-
-(use-fixtures :each setup-clojure)
+            [heuhaufen.core :refer :all]))
 
 (deftest a-test
-  (testing "Storing and retrieving things with Lucene"
-    (is (= (clucy/search index "bob" 10) '({:name "Bob", :job "Builder"})))))
+  (testing "I succeed."
+    (is (= 1 1))))
---
+++ test/heuhaufen/search_test.clj
@@ -0,0 +1,20 @@
+(ns heuhaufen.search-test
+  (:require [clojure.test :refer :all]
+            [heuhaufen.core :refer :all]
+            [clucy.core :as clucy]))
+
+(defn setup-clojure
+  [f]
+  (def index (clucy/memory-index))
+  (clucy/add index
+       {:name "Bob", :job "Builder"}
+       {:name "Donald", :job "Computer Scientist"})
+  (f)
+  )
+
+(use-fixtures :each setup-clojure)
+
+(deftest a-test
+  (testing "Storing and retrieving things with Lucene"
+    (is (= (clucy/search index "bob" 10) '({:name "Bob", :job "Builder"})))))
+
